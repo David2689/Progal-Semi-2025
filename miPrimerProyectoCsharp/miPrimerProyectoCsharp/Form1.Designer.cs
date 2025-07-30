e18454f66f7275eb7f7b3d90b547cfe2936c80ac
David <drodriguez2605@gamil.com>
2025-07-30 10:35:10 -0600

    calculadora basica de suma de dos numeros

--- miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.Designer.cs
+++ miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.Designer.cs
@@ -28,27 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.lblnum1 = new System.Windows.Forms.Label();
+            this.btn = new System.Windows.Forms.Button();
+            this.lblNUm2 = new System.Windows.Forms.Label();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.lblRespuesta = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // txtNum1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.label1.Location = new System.Drawing.Point(141, 136);
-            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(105, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Hola Mundo";
+            this.txtNum1.Location = new System.Drawing.Point(99, 42);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 24);
+            this.txtNum1.TabIndex = 0;
+            // 
+            // lblnum1
+            // 
+            this.lblnum1.AutoSize = true;
+            this.lblnum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnum1.Location = new System.Drawing.Point(12, 46);
+            this.lblnum1.Name = "lblnum1";
+            this.lblnum1.Size = new System.Drawing.Size(55, 20);
+            this.lblnum1.TabIndex = 1;
+            this.lblnum1.Text = "Num 1";
+            // 
+            // btn
+            // 
+            this.btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn.Location = new System.Drawing.Point(99, 217);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(88, 49);
+            this.btn.TabIndex = 2;
+            this.btn.Text = "Calcular";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // lblNUm2
+            // 
+            this.lblNUm2.AutoSize = true;
+            this.lblNUm2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNUm2.Location = new System.Drawing.Point(12, 112);
+            this.lblNUm2.Name = "lblNUm2";
+            this.lblNUm2.Size = new System.Drawing.Size(55, 20);
+            this.lblNUm2.TabIndex = 4;
+            this.lblNUm2.Text = "Num 2";
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(99, 108);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 24);
+            this.txtNum2.TabIndex = 3;
+            // 
+            // lblRespuesta
+            // 
+            this.lblRespuesta.AutoSize = true;
+            this.lblRespuesta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRespuesta.Location = new System.Drawing.Point(12, 169);
+            this.lblRespuesta.Name = "lblRespuesta";
+            this.lblRespuesta.Size = new System.Drawing.Size(96, 20);
+            this.lblRespuesta.TabIndex = 5;
+            this.lblRespuesta.Text = "Respuesta?";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(519, 425);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(339, 295);
+            this.Controls.Add(this.lblRespuesta);
+            this.Controls.Add(this.lblNUm2);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.lblnum1);
+            this.Controls.Add(this.txtNum1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.Name = "Form1";
@@ -60,7 +113,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.Label lblnum1;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label lblNUm2;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Label lblRespuesta;
     }
 }
 
