6aa901f437ad5225653b46b3fd30240e914015ee
David <drodriguez2605@gamil.com>
2025-09-23 07:49:17 -0300

    Creacion de la rama BasedeDatosMaestros

--- miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.Designer.cs
+++ miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.Designer.cs
@@ -29,153 +29,23 @@
         private void InitializeComponent()
         {
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.cmbDestino = new System.Windows.Forms.ComboBox();
-            this.lblSeleccionelaunidaddedestino = new System.Windows.Forms.Label();
-            this.lblResultado = new System.Windows.Forms.Label();
-            this.btnConvertir = new System.Windows.Forms.Button();
-            this.cmbOrigen = new System.Windows.Forms.ComboBox();
-            this.lblSeleccioneunidad = new System.Windows.Forms.Label();
-            this.txtValor = new System.Windows.Forms.TextBox();
-            this.lblIngreseValor = new System.Windows.Forms.Label();
-            this.lblConversordearea = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // cmbDestino
-            // 
-            this.cmbDestino.FormattingEnabled = true;
-            this.cmbDestino.Items.AddRange(new object[] {
-            "Pie Cuadrado",
-            "Vara Cuadrada",
-            "Yarda Cuadrada",
-            "Metro Cuadrado",
-            "Tareas",
-            "Manzana",
-            "Hectárea ",
-            ""});
-            this.cmbDestino.Location = new System.Drawing.Point(311, 185);
-            this.cmbDestino.Name = "cmbDestino";
-            this.cmbDestino.Size = new System.Drawing.Size(155, 26);
-            this.cmbDestino.TabIndex = 18;
-            // 
-            // lblSeleccionelaunidaddedestino
-            // 
-            this.lblSeleccionelaunidaddedestino.AutoSize = true;
-            this.lblSeleccionelaunidaddedestino.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSeleccionelaunidaddedestino.Location = new System.Drawing.Point(53, 185);
-            this.lblSeleccionelaunidaddedestino.Name = "lblSeleccionelaunidaddedestino";
-            this.lblSeleccionelaunidaddedestino.Size = new System.Drawing.Size(230, 16);
-            this.lblSeleccionelaunidaddedestino.TabIndex = 17;
-            this.lblSeleccionelaunidaddedestino.Text = "Seleccione la unidad de destino";
-            // 
-            // lblResultado
-            // 
-            this.lblResultado.AutoSize = true;
-            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblResultado.Location = new System.Drawing.Point(65, 229);
-            this.lblResultado.Name = "lblResultado";
-            this.lblResultado.Size = new System.Drawing.Size(78, 16);
-            this.lblResultado.TabIndex = 16;
-            this.lblResultado.Text = "Relsutado";
-            // 
-            // btnConvertir
-            // 
-            this.btnConvertir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnConvertir.Location = new System.Drawing.Point(300, 229);
-            this.btnConvertir.Name = "btnConvertir";
-            this.btnConvertir.Size = new System.Drawing.Size(178, 87);
-            this.btnConvertir.TabIndex = 15;
-            this.btnConvertir.Text = "Convertir";
-            this.btnConvertir.UseVisualStyleBackColor = true;
-            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
-            // 
-            // cmbOrigen
-            // 
-            this.cmbOrigen.FormattingEnabled = true;
-            this.cmbOrigen.Items.AddRange(new object[] {
-            "Pie Cuadrado",
-            "Vara Cuadrada",
-            "Yarda Cuadrada",
-            "Metro Cuadrado",
-            "Tareas",
-            "Manzana",
-            "Hectárea ",
-            ""});
-            this.cmbOrigen.Location = new System.Drawing.Point(245, 136);
-            this.cmbOrigen.Name = "cmbOrigen";
-            this.cmbOrigen.Size = new System.Drawing.Size(155, 26);
-            this.cmbOrigen.TabIndex = 14;
-            // 
-            // lblSeleccioneunidad
-            // 
-            this.lblSeleccioneunidad.AutoSize = true;
-            this.lblSeleccioneunidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSeleccioneunidad.Location = new System.Drawing.Point(65, 136);
-            this.lblSeleccioneunidad.Name = "lblSeleccioneunidad";
-            this.lblSeleccioneunidad.Size = new System.Drawing.Size(153, 16);
-            this.lblSeleccioneunidad.TabIndex = 13;
-            this.lblSeleccioneunidad.Text = "Seleccione la unidad";
-            // 
-            // txtValor
-            // 
-            this.txtValor.Location = new System.Drawing.Point(220, 85);
-            this.txtValor.Name = "txtValor";
-            this.txtValor.Size = new System.Drawing.Size(158, 24);
-            this.txtValor.TabIndex = 12;
-            // 
-            // lblIngreseValor
-            // 
-            this.lblIngreseValor.AutoSize = true;
-            this.lblIngreseValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblIngreseValor.Location = new System.Drawing.Point(86, 91);
-            this.lblIngreseValor.Name = "lblIngreseValor";
-            this.lblIngreseValor.Size = new System.Drawing.Size(107, 18);
-            this.lblIngreseValor.TabIndex = 11;
-            this.lblIngreseValor.Text = "Ingrese Valor";
-            // 
-            // lblConversordearea
-            // 
-            this.lblConversordearea.AutoSize = true;
-            this.lblConversordearea.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblConversordearea.Location = new System.Drawing.Point(183, 25);
-            this.lblConversordearea.Name = "lblConversordearea";
-            this.lblConversordearea.Size = new System.Drawing.Size(270, 31);
-            this.lblConversordearea.TabIndex = 10;
-            this.lblConversordearea.Text = "Conversor de areas";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(742, 346);
-            this.Controls.Add(this.cmbDestino);
-            this.Controls.Add(this.lblSeleccionelaunidaddedestino);
-            this.Controls.Add(this.lblResultado);
-            this.Controls.Add(this.btnConvertir);
-            this.Controls.Add(this.cmbOrigen);
-            this.Controls.Add(this.lblSeleccioneunidad);
-            this.Controls.Add(this.txtValor);
-            this.Controls.Add(this.lblIngreseValor);
-            this.Controls.Add(this.lblConversordearea);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "BasedeDatosMaestros";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
-        private System.Windows.Forms.ComboBox cmbDestino;
-        private System.Windows.Forms.Label lblSeleccionelaunidaddedestino;
-        private System.Windows.Forms.Label lblResultado;
-        private System.Windows.Forms.Button btnConvertir;
-        private System.Windows.Forms.ComboBox cmbOrigen;
-        private System.Windows.Forms.Label lblSeleccioneunidad;
-        private System.Windows.Forms.TextBox txtValor;
-        private System.Windows.Forms.Label lblIngreseValor;
-        private System.Windows.Forms.Label lblConversordearea;
     }
 }
 
--- miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.cs
+++ miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.cs
@@ -17,67 +17,6 @@ namespace miPrimerProyectoCsharp
         public Form1()
         {
             InitializeComponent();
-            CargarUnidades();
-        }
-        private readonly string[] unidades = {
-            "Pie cuadrado",
-            "Vara cuadrada",
-            "Yarda cuadrada",
-            "Metro cuadrado",
-            "Tareas",
-            "Manzana",
-            "Hectárea"
-        };
-
-        private readonly double[] factores = {
-            0.092903, // Pie cuadrado
-            0.698737, // Vara cuadrada
-            0.836127, // Yarda cuadrada
-            1,        // Metro cuadrado
-            437.5,    // Tareas
-            6987,     // Manzana
-            10000     // Hectárea
-        };
-        private void CargarUnidades()
-        {
-            cmbOrigen.Items.Clear();
-            cmbDestino.Items.Clear();
-            cmbOrigen.Items.AddRange(unidades);
-            cmbDestino.Items.AddRange(unidades);
-
-            cmbOrigen.DropDownStyle = ComboBoxStyle.DropDownList;
-            cmbDestino.DropDownStyle = ComboBoxStyle.DropDownList;
-
-            cmbOrigen.SelectedIndex = 3;  // Metro cuadrado
-            cmbDestino.SelectedIndex = 0; // Pie cuadrado
-        }
-        private double Convertir(double valor, int origen, int destino)
-        {
-            double valorEnMetros = valor * factores[origen]; // ✅ usa la matriz
-            return valorEnMetros / factores[destino];
-        }
-
-        private void btnConvertir_Click(object sender, EventArgs e)
-        {
-            if (!double.TryParse(txtValor.Text, out double valor))
-            {
-                MessageBox.Show("Ingrese un valor numérico válido.");
-                txtValor.Focus();
-                return;
-            }
-
-            if (cmbOrigen.SelectedIndex < 0 || cmbDestino.SelectedIndex < 0)
-            {
-                MessageBox.Show("Seleccione las unidades de origen y destino.");
-                return;
-            }
-
-            int iOrigen = cmbOrigen.SelectedIndex;
-            int iDestino = cmbDestino.SelectedIndex;
-
-            double res = Convertir(valor, iOrigen, iDestino);
-
-            lblResultado.Text = $"{res:N4} {unidades[iDestino]}";
         }
     }
 }
